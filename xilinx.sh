228907f9f83dc875776c335864a9e8c04b970212
wxx <742418897@qq.com>
2014-11-02 19:21:17 +0800

    xilinx.sh

---
+++ xilinx.sh
@@ -0,0 +1,6 @@
+source /home/rebecca/sw/Xilinx/14.7/ISE_DS/settings64.sh
+source /home/rebecca/sw/petalinux-v2013.10-final/settings.sh
+alias ise="sudo /home/rebecca/sw/Xilinx/14.7/ISE_DS/ISE/bin/lin64/ise"
+alias xsdk="sudo /home/rebecca/sw/Xilinx/14.7/ISE_DS/EDK/bin/lin64/xsdk"
+alias planahead="sudo /home/rebecca/sw/Xilinx/14.7/ISE_DS/PlanAhead/bin/planAhead"
+export mm_path="root@192.168.169.101"
